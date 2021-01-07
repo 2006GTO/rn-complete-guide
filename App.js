84d88d6b6d97c99e25f4126e2d820c707b01a3e8
2006GTO <23041534+2006GTO@users.noreply.github.com>
2021-01-07 17:00:17 -0600

    added flatlist

--- App.js
+++ App.js
@@ -1,35 +1,75 @@
-import React, { useState } from "react";
-import { StyleSheet, View, Button, TextInput } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Button,
+  Text,
+  TextInput,
+  ScrollView,
+  FlatList,
+} from "react-native";
 
 export default function App() {
-  const [dynamicText, setDynamicText] = useState("Click to change");
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  useEffect(() => {}, []);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addCourseGoal = () => {
+    setCourseGoals((currentGoals) => [
+      ...currentGoals,
+      { key: Math.random().toString, value: enteredGoal },
+    ]);
+  };
+
   return (
-    <View
-      style={{
-        padding: 50,
-        flexDirection: "row",
-      }}
-    >
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "center",
-        }}
-      >
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
         <TextInput
-          placeholder={"A Nice Placeholder"}
-          style={{
-            borderBottomColor: "black",
-            borderBottomWidth: 2,
-          }}
+          placeholder={"input here"}
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
         />
-        <Button
-          title="Change Text"
-          onPress={() => setDynamicText("you clicked it!")}
-        />
+        <Button title="Change Text" onPress={addCourseGoal} />
       </View>
+      <FlatList
+        data={courseGoals}
+        renderItem={(itemData) => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      ></FlatList>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  input: {
+    width: "60%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
+  },
+});
